007fa2d2477d721e0fa0a11845dff1335c158cc8
Endolite <41879690+Endolite@users.noreply.github.com>
2025-04-13 03:32:57 -0400

    Initial commit

---
+++ languages/sil/highlights.scm
@@ -0,0 +1,11 @@
+[(true) (false)] @boolean
+[(not) (logic_bin)] @operator
+
+[(begin) (end)] @label
+
+[(skip) (if) (then) (else) (while) (do) (procedure) (array) (class) (method) (new) (print)] @keyword
+
+(var) @variable
+(fun) @function
+
+(comment) @comment
